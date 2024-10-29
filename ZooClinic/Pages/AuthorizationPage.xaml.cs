5b9c6498acc77ddd670e8cebbdc408486d190f78
202119 <202119@1305-003>
2024-10-29 15:06:44 +0300

    Добавьте файлы проекта.

---
+++ ZooClinic/Pages/AuthorizationPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ZooClinic.DB;
+
+namespace ZooClinic.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationPage.xaml
+    /// </summary>
+    public partial class AuthorizationPage : Page
+    {
+        public AuthorizationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterBt_Click(object sender, RoutedEventArgs e)
+        {
+            List<Veterinarians> veterinarians = Connection.clinic.Veterinarians.ToList();
+            App.authUser = veterinarians.FirstOrDefault(i => i.Login == LoginTb.Text && i.Password == PasswordTb.Password);
+            if (App.authUser != null && App.authUser.IdSpecialty == 1)
+            {
+                NavigationService.Navigate(new RegisterPage());
+            }
+            else if (App.authUser != null && App.authUser.IdSpecialty != 1)
+            {
+                NavigationService.Navigate(new MainMenuPage());
+            }
+            else
+            {
+                MessageBox.Show("Неверные данные!");
+            }
+
+        }
+
+       
+    }
+}
